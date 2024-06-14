f64f3ac0e986138210e4a0ce4a8de60ff230f400
User <user@Users-MacBook-Air.local>
2024-06-14 11:34:48 -0300

    carrito hs

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,67 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let carrito = JSON.parse(localStorage.getItem("carrito"));
+    let contenido = document.querySelector(".contenedor");
+
+    if (!carrito || carrito.length === 0) {
+        contenido.innerHTML = "<p>El carrito está vacío</p>";
+    } else {
+        let sectionContenido = document.querySelector(".contenedor");
+        let articulo = ""; 
+
+        for (let i = 0; i < carrito.length; i++) {
+            let productId = carrito[i]; 
+
+            fetch(`https://fakestoreapi.com/products/${productId}`)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+
+                    articulo += `
+                        <article class="elementos">
+                            <h3>${data.title}</h3>
+                            <img class="imagen" src="${data.image}" alt="${data.title}">
+                            <p>${data.description}</p>
+                            <p>$ ${data.price}</p>
+                            <a href="./cart.html"><img class="icono" src="./IMG/shopping-cart-free-png.webp" alt="carro"></a>
+                            <p><a href="./producto.html?id=${data.id}">Ver más</a></p>
+                        </article>
+                    `;
+
+                    sectionContenido.innerHTML = articulo;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+
+        let finalizarCompraLink = document.querySelector(".finalizar");
+        
+        if (finalizarCompraLink) {
+            finalizarCompraLink.addEventListener("click", function(event) {
+                event.preventDefault();
+
+                
+                localStorage.removeItem("carrito");
+
+                
+                alert("¡Gracias por tu compra!");
+
+                
+                window.location.href = "./index.html";
+            });
+        }
+    }
+});
+
+
+
+
+
+
+
+
 //////navegador 
 fetch("https://fakestoreapi.com/products/categories")
 .then(function(res) {
