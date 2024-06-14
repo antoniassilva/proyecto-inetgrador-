30acde8cea8a51486c5e0dba3c5592669275f258
User <user@Users-MacBook-Air.local>
2024-06-14 10:00:40 -0300

    recover js

---
+++ js/recover.js
@@ -0,0 +1,23 @@
+let campoEmail = document.querySelector('#email');
+
+let campoTerminos = document.querySelector('#terminos');
+
+let formulario = document.querySelector('form');
+
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    if (campoEmail.value === '') {
+        alert("Por favor complete el campo correo!");
+    } 
+    
+    else if (!campoTerminos.checked) {
+        alert("Acepte los términos y condiciones");
+    } 
+    else {
+       
+        formulario.submit(); 
+        alert("Recibiras un mail con las instrucciones para recuperar tu contraseña")
+        window.location.href = "./login.html"; 
+    }
+});
